fd9161ca86bde6364b0110ff4603c81cf96ecafa
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-04 14:42:01 +0000

    Feat: takes number list input return string csv with Fizz and Buzz and FizzBuzz for [1 .. 15]

--- src/test/java/NumbersConvertedToFizzBuzzStringTests.java
+++ src/test/java/NumbersConvertedToFizzBuzzStringTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -12,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class NumbersConvertedToFizzBuzzStringTests {
     @Test
     @Parameters(
-           method = "parameters_for_converts_list_of_numbers_to_csv_string"
+            method = "parameters_for_converts_list_of_numbers_to_csv_string"
     )
     public void converts_list_of_numbers_to_csv_string(String expectedOutput, List<Integer> numberList) {
         assertEquals(expectedOutput, new FizzBuzz(numberList).generate());
@@ -23,6 +25,7 @@ public class NumbersConvertedToFizzBuzzStringTests {
                 new Object[]{"1, 2", asList(1, 2)},
                 new Object[]{"1, 2, Fizz", asList(1, 2, 3)},
                 new Object[]{"1, 2, Fizz, 4, Buzz", asList(1, 2, 3, 4, 5)},
+                new Object[]{"1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)},
         };
     }
 }
