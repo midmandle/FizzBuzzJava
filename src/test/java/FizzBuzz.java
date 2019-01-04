4d91f87a98c3f74c93d41d3d6f3cc3e24f185462
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-01-04 11:35:39 +0000

    Refactor: applied extract method for if number is divisible by three

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -10,12 +10,16 @@ public class FizzBuzz {
 
     public String generate() {
         Integer currentNumber = numberList.get(0);
-        if (currentNumber % 3 == 0 && currentNumber % 5 == 0)
+        if (isDivisibleBy3(currentNumber) && currentNumber % 5 == 0)
             return "FizzBuzz";
         if (currentNumber % 5 == 0)
             return "Buzz";
-        if (currentNumber % 3 == 0)
+        if (isDivisibleBy3(currentNumber))
             return "Fizz";
         return currentNumber.toString();
+    }
+
+    private boolean isDivisibleBy3(Integer currentNumber) {
+        return currentNumber % 3 == 0;
     }
 }
